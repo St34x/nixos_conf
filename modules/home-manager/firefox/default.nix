cdc1d60c3fa6fc7a6953844ad839ad45af285864
St34x <mykola.vradnyk@ecole2600.com>
2024-05-04 18:32:17 +0200

    firefor conf

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -6,7 +6,7 @@
     firefox = {
       enable 				= true;
       policies 				= import ./firefox-policies.nix;
-      extraConfig = with config.colorScheme.palette = {
+      extraConfig = with config.colorScheme.palette; {
 	bg 				= "#${base00}";
 	icon-color 			= "#${base06}";
 	sidebar-bg 			= "#${base00}";
