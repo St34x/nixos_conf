f0dcc97190bfe6d5688ff1093edf09152f5ec404
St34x <mykola.vradnyk@ecole2600.com>
2024-05-20 22:49:55 +0200

    stylix

--- nixos/stylix.nix
+++ nixos/stylix.nix
@@ -1,6 +1,6 @@
 { pkgs, ...}:
 {
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-storm.yaml";
-  stylix.image = /home/st34x/.wallpapers/wall-06.webp;
-  stylix.polarity = "dark";
+  stylix.base16Scheme 	= "${pkgs.base16-schemes}/share/themes/tokyo-night-storm.yaml";
+  stylix.image 		= /home/st34x/.wallpapers/space.png;
+  stylix.polarity 	= "dark";
 }
